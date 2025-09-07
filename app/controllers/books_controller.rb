e5feb93f4434db1d826bd9dd598585f1bada7651
susuki-m <212071147+susuki-m@users.noreply.github.com>
2025-09-07 16:11:14 +0000

    [add]showとeditのhtml入力

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,25 @@
 class BooksController < ApplicationController
   def new
     @book = Book.new
+    @books = Book.all
   end
 
   def create
-    book =Book.new(book_params)
+    @book =Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to '/books/:id'
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   private
