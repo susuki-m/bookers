f9ea8a4a352787292c0fc279a0a9a07e7e873a13
susuki-m <212071147+susuki-m@users.noreply.github.com>
2025-09-08 13:44:11 +0000

    [add]show/edit/update/destroy追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-    @books = Book.all
-  end
 
   def create
     @book =Book.new(book_params)
@@ -12,6 +8,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -20,6 +17,18 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books/new'
   end
 
   private
