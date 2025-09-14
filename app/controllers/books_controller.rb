9c5f923762e058351c2afd4487ca1691140af90e
susuki-m <212071147+susuki-m@users.noreply.github.com>
2025-09-14 16:51:29 +0000

    [add]一通りの機能搭載完了　テスト未完

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
 
   def create
-    @book =Book.new(book_params)
+    @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "successfully"
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       flash.now[:alert] = "error"
       render :index
     end
@@ -25,11 +26,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "successfully"
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
+      @books = Book.all
       flash.now[:alert] = "error"
       render :edit
     end
